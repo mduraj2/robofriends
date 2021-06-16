f44b6e1ba184dad29f7948a3544c19b53a06b780
mirek <m.duraj2@wp.pl>
2021-06-16 22:13:51 +0200

    uploading new files

---
+++ src/robots.js
@@ -0,0 +1,80 @@
+export const robots = [
+  {
+    id: 14,
+    name: 'Jozef Woronowski',
+    username: 'Jozef.Woronoski',
+    email: 'jozef.woronowski@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Krystyna Woronowska',
+    username: 'Krystyna.Woronoska',
+    email: 'krystyna.woronowska@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Stanisława Duraj',
+    username: 'Stanisława.Duraj',
+    email: 'stanisława.duraj@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Bronisław Duraj',
+    username: 'Bronisław.Duraj',
+    email: 'bronisław.duraj@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Magdalena de Rooy',
+    username: 'Magdalena.deRooy',
+    email: 'magdalena.derooy@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Jeroen de Rooy',
+    username: 'Jeroen.deRooy',
+    email: 'jeroen.derooy@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Anna de Rooy',
+    username: 'Anna.deRooy',
+    email: 'anna.derooy@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Alicja Wrzosek',
+    username: 'Alicja.Wrzosek',
+    email: 'alicja.wrzosek@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Damian Wrzosek',
+    username: 'Damian.Wrzosek',
+    email: 'damian.wrzosek@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Tomasz Wrzosek',
+    username: 'Tomasz.Wrzosek',
+    email: 'tomasz.wrzosek@gmail.com'
+  },
+  {
+    id: 11,
+    name: 'Miroslaw Duraj',
+    username: 'Miroslaw.Duraj',
+    email: 'miroslaw.duraj@syncreon.com'
+  },
+  {
+    id: 13,
+    name: 'Sylwia Duraj',
+    username: 'Sylwia.Duraj',
+    email: 'sylwia.duraj@syncreon.com'
+  },
+  {
+    id: 1,
+    name: 'Oscar Duraj',
+    username: 'Oscar.Duraj',
+    email: 'o.j.duraj@gmail.com'
+  },
+];
